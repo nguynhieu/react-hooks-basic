079462c7648a8acd03dba3daea579f6b26c187c4
Nguyen Hieu <tthzicc@gmail.com>
2020-06-09 15:29:11 +0700

    First demo for react hooks

--- Desktop/react-hooks-basic/my-app/src/App.js
+++ Desktop/react-hooks-basic/my-app/src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const originalColor = localStorage.getItem('backgroundColor') || 'pink';
+  const colorList = ['pink', 'blue', 'green', 'red'];
+  const [backgroundColor, setColor] = useState(originalColor);
+
+  function handleSetcolor() {
+    const randomColor = colorList[Math.floor(Math.random() * colorList.length)];
+    localStorage.setItem('backgroundColor', randomColor);
+    const backgroundColor = localStorage.getItem('backgroundColor');
+    setColor(backgroundColor);
+  }
+
   return (
     <div className="app">
+      <div onClick={() => handleSetcolor()} className="box" style={{ background: backgroundColor }}>
+
+      </div>
     </div>
   );
 }
